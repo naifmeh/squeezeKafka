a06de78938cb83bfa6007dfc25f8df762287497c
tarzan212 <activation212@gmail.com>
2018-07-17 17:22:45 -0300

    modified init script

--- initKafka.sh
+++ initKafka.sh
@@ -13,7 +13,7 @@ if [ "$2" == "start" ];then
     `$KAFKA_PATH/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic training-img-topic`
     `$KAFKA_PATH/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic classifier-pkl-topic`
 
-    `$KAFKA_PATH/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic video-stream-topic`
+    `$KAFKA_PATH/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic video-stream-topic --config retention.ms=3000 retention.bytes=100000000`
     `$KAFKA_PATH/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic video-result-topic`
 elif [ "$2" == "delete" ]; then
 
